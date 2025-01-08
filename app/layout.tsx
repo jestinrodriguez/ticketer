6e397b37e976158df026d4320fb83b1d5f00ed97
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2025-01-08 05:31:39 -0800

    started header component and finished searchbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 import { ConvexReactClient } from "convex/react";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
       <ConvexClientProvider>
         <ClerkProvider>
+          <Header/>
           {children}
         </ClerkProvider>
       </ConvexClientProvider>
