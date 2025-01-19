b13b738cb93d1bd4091daf0c71b5cb8ebf81a765
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2025-01-18 16:10:18 -0800

    built create event functionality and EventForm component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "determined-parrot-937.convex.cloud",
+        protocol: "https"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
